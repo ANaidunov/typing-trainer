93eeb33e770a36fc8adcbbefab31526b1d59c936
Anton Naidunou <Anton_Naidunou@epam.com>
2022-09-27 11:24:58 +0200

    Fix space highlight

--- src/app/store/keyboard/keyboard.reducer.ts
+++ src/app/store/keyboard/keyboard.reducer.ts
@@ -1,20 +1,20 @@
 import { createReducer, on } from "@ngrx/store";
-import { KeyboardKey } from "src/components/keyboard-monitor/keyboard-monitor.component";
 import { setNextTypedKey } from "./keyboard.actions";
 import { initialState, KeyboardState } from "./keyboard.state";
 
 export const keyboardReducer = createReducer(initialState,
   on(setNextTypedKey, (state: KeyboardState, { pressedKey }) => {
+    const nextPressedKeyName = pressedKey === ' ' ? 'Space' : pressedKey;
     const keyRows = state.keyRows.map(row => {
       const newRow = [...row];
-      const pressedKeyIndex = row.findIndex(key => key.key === pressedKey);
-      const pressedKeyOnKeyboard = newRow.find(key => key.key === pressedKey);
+      const pressedKeyIndex = newRow.findIndex(key => key.key === nextPressedKeyName);
+      const pressedKeyOnKeyboard = newRow[pressedKeyIndex];
 
       if (pressedKeyOnKeyboard) {
         newRow[pressedKeyIndex] = { ...pressedKeyOnKeyboard, isHighlighted: true };
       }
 
-      return pressedKeyOnKeyboard ? [...newRow] : row;
+      return pressedKeyOnKeyboard ? newRow : row;
     });
 
     const newState = {
