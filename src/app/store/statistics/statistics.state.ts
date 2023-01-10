e9c2bd5e5ccea51685b0cdfee4de8887eb7e026a
Anton Naidunou <Anton_Naidunou@epam.com>
2023-01-10 13:00:02 +0300

    Add state for progress bar

--- src/app/store/statistics/statistics.state.ts
+++ src/app/store/statistics/statistics.state.ts
@@ -3,13 +3,16 @@ import { createFeatureSelector, createReducer, createSelector } from "@ngrx/stor
 export interface StatisticsState {
   time: number;
   wordsLength: number;
+  currentSymbolsCount: number;
 };
 
 export const initialState: StatisticsState = {
   time: 0,
   wordsLength: 0,
+  currentSymbolsCount: 0,
 };
 
 export const featureSelector = createFeatureSelector<StatisticsState>('statistics');
 
 export const wordsLengthSelector = createSelector(featureSelector, state => state.wordsLength);
+export const currentSymbolsCountSelector = createSelector(featureSelector, state => state.currentSymbolsCount);
